a3d1025f230fa9904cf8bda49882b667dcd09be3
Lola <lolahef@gmail.com>
2019-04-18 13:12:06 -0400

    Alternative rps solution

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,8 +4,22 @@ import sys
 import itertools
 import functools
 
+# def rock_paper_scissors(n):
+#   return [list(x) for x in itertools.product(["rock", "paper", "scissors"], repeat=n)]
+
 def rock_paper_scissors(n):
-  return [list(x) for x in itertools.product(["rock", "paper", "scissors"], repeat=n)]
+  options = ['rock', 'paper', 'scissors']
+  plays = []
+
+  def generate_plays(num, outcome=[]):
+    if num != 0:
+      for option in options:
+        generate_plays(num - 1, outcome + [option])
+    else:
+      plays.append(outcome)
+  
+  generate_plays(n)
+  return plays
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
